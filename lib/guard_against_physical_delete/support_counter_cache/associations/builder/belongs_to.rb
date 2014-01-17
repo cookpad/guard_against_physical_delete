0d0b9be3ca5ee20952b42cdb0c60728090c097b4
Akira Matsuda <ronnie@dio.jp>
2014-01-17 21:20:29 +0900

    Prefer updating the counter_cache in after_update rather than before as AR4 does

--- lib/guard_against_physical_delete/support_counter_cache/associations/builder/belongs_to.rb
+++ lib/guard_against_physical_delete/support_counter_cache/associations/builder/belongs_to.rb
@@ -16,7 +16,7 @@ module GuardAgainstPhysicalDelete
                 cache_column = reflection.counter_cache_column
                 name = self.name
 
-                logical_delete_method_name = "belongs_to_counter_cache_before_logical_delete_for_#{name}".to_sym
+                logical_delete_method_name = "belongs_to_counter_cache_after_logical_delete_for_#{name}".to_sym
                 mixin.redefine_method(logical_delete_method_name) do
                   delete_column = self.logical_delete_column
                   next unless send("#{delete_column}_changed?")
@@ -29,7 +29,7 @@ module GuardAgainstPhysicalDelete
                   # do nothing
                 end if mixin.method_defined?("belongs_to_counter_cache_after_update_for_#{name}")
 
-                revive_method_name = "belongs_to_counter_cache_before_revive_for_#{name}".to_sym
+                revive_method_name = "belongs_to_counter_cache_after_revive_for_#{name}".to_sym
 
                 mixin.redefine_method(revive_method_name) do
                   delete_column = self.logical_delete_column
@@ -39,7 +39,7 @@ module GuardAgainstPhysicalDelete
                   record = send(name)
                   record.class.increment_counter(cache_column, record.id) unless record.nil?
                 end
-                model.before_update(logical_delete_method_name, revive_method_name)
+                model.after_update(logical_delete_method_name, revive_method_name)
               end
             end
           end
