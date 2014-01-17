dd419b377074286fcae91419fc0d3a58095e12ac
Akira Matsuda <ronnie@dio.jp>
2014-01-17 21:42:42 +0900

    Stand by for AR 4.1

--- lib/guard_against_physical_delete/support_counter_cache.rb
+++ lib/guard_against_physical_delete/support_counter_cache.rb
@@ -1,5 +1,5 @@
 if ActiveRecord::VERSION::STRING >= "3.2"
-  require 'guard_against_physical_delete/support_counter_cache/associations/builder/belongs_to'
+  require 'guard_against_physical_delete/support_counter_cache/3.2_and_4.0/associations/builder/belongs_to'
   ActiveRecord::Associations::Builder::BelongsTo.send(:include, GuardAgainstPhysicalDelete::SupportCounterCache::Associations::Builder::BelongsTo)
 elsif ActiveRecord::VERSION::STRING == "3.0.10"
   require 'guard_against_physical_delete/support_counter_cache/3.0.10/associations'
