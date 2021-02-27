b4c4a41b01d1f959e69a27e0ceb192c7245a9130
Tomoki Aonuma <uasi@uasi.jp>
2021-02-27 16:41:07 +0900

    Support activerecord 5.1

--- lib/guard_against_physical_delete/support_counter_cache/associations/builder/belongs_to.rb
+++ lib/guard_against_physical_delete/support_counter_cache/associations/builder/belongs_to.rb
@@ -14,8 +14,8 @@ module GuardAgainstPhysicalDelete
                   def belongs_to_counter_cache_after_logical_delete(reflection)
                     cache_column = reflection.counter_cache_column
                     delete_column = self.logical_delete_column
-                    return unless send("#{delete_column}_changed?")
-                    return unless send("#{delete_column}_was").nil?
+                    return unless saved_change_to_logical_delete_column?(delete_column)
+                    return unless logical_delete_column_before_last_save(delete_column).nil?
 
                     record = send(reflection.name)
                     record.class.decrement_counter(cache_column, record.id) unless record.nil?
@@ -24,13 +24,32 @@ module GuardAgainstPhysicalDelete
                   def belongs_to_counter_cache_after_revive(reflection)
                     cache_column = reflection.counter_cache_column
                     delete_column = self.logical_delete_column
-                    return unless send("#{delete_column}_changed?")
-                    return if send("#{delete_column}_was").nil?
+                    return unless saved_change_to_logical_delete_column?(delete_column)
+                    return if logical_delete_column_before_last_save(delete_column).nil?
                     return unless send(delete_column).nil?
                     record = send(reflection.name)
                     record.class.increment_counter(cache_column, record.id) unless record.nil?
                   end
+
+                  if ActiveRecord.version < Gem::Version.new('5.1.0')
+                    def saved_change_to_logical_delete_column?(delete_column)
+                      __send__("#{delete_column}_changed?")
+                    end
+
+                    def logical_delete_column_before_last_save(delete_column)
+                      __send__("#{delete_column}_was")
+                    end
+                  else
+                    def saved_change_to_logical_delete_column?(delete_column)
+                      __send__("saved_change_to_#{delete_column}")
+                    end
+
+                    def logical_delete_column_before_last_save(delete_column)
+                      __send__("#{delete_column}_before_last_save")
+                    end
+                  end
                 end
+
                 mixin.redefine_method("belongs_to_counter_cache_after_update_for_#{name}") do
                   # do nothing
                 end if mixin.method_defined?("belongs_to_counter_cache_after_update_for_#{name}")
